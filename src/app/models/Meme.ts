5e96d9aae16954b98a77549831ff458001b376a3
leoee <leonardoe1997@gmail.com>
2020-12-19 09:51:31 -0300

    Add Meme model

---
+++ src/app/models/Meme.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from 'mongoose'
+
+const MemeSchema = new Schema(
+	{
+		titulo: String,
+		descricao: String,
+		ano: Number,
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export default model('Meme', MemeSchema)
